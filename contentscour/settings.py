fcbc355028e1c567bc4e77a0c8eabf3b17d4fd16
Marcelo Silva <mhenrique94@outlook.com>
2024-06-01 19:45:37 -0300

    sets cors middleware

--- contentscour/settings.py
+++ contentscour/settings.py
@@ -54,7 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'contentscour.urls'
